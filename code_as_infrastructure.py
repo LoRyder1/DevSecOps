9030d5afe0ddf5e21e608f09a2a51cf8e8ba7ccf
L Pnag <lspnag@gmail.com>
2025-04-28 21:30:12 -0400

    create public subnet, route table
    also associate subnet to table

--- code_as_infrastructure.py
+++ code_as_infrastructure.py
@@ -22,3 +22,28 @@ internet_gateway = aws.ec2.InternetGateway("my-igw",
         "Name": "my-internet-gateway",
     })
 
+# 3. Create a Public Subnet
+public_subnet = aws.ec2.Subnet("public-subnet-a",
+    vpc_id=vpc.id,
+    cidr_block="10.0.1.0/24",
+    availability_zone="us-east-1a",
+    map_public_ip_on_launch=True,
+    tags={
+        "Name": "public-subnet-a",
+    })
+
+#4. Create a Route Table for the Public Subnet
+public_route_table = aws.ec2.RouteTable("public-route-table",
+    vpc_id=vpc.id,
+    routes=[aws.ec2.RouteTableRouteArgs(
+        cidr_block="0.0.0.0/0",
+        gateway_id=internet_gateway.id,
+    )],
+    tags={
+        "Name": "public-route-table",
+    })
+
+#5. Associate the Public Subnet with the Public Route Table
+public_route_table_association = aws.ec2.RoutTableAssociation("public-subnet-a-association", 
+    subnet_id=public_subnet.id,
+    route_table_id=public_route_table.id)
